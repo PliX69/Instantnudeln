dd402bd5d993fdb990e2a8aae39d29b73a5f9fab
Paul <117029448+PliX69@users.noreply.github.com>
2023-03-22 14:40:01 +0100

    Update capy.js

--- capy.js
+++ capy.js
@@ -1,7 +1,6 @@
-// Get the navigation links
+
 const navLinks = document.querySelectorAll("nav a");
 
-// Add event listeners to each link to scroll to the appropriate section
 navLinks.forEach((link) => {
   link.addEventListener("click", (event) => {
     event.preventDefault();
